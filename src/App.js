bc495a1494d5cac54052d7cf1a195320c5e9c3bc
Jeffrey France <jbfrance1626@gmail.com>
2020-04-04 23:19:59 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
+
+// Components
+import Navbar from './components/layout/Navbar';
+import SearchBar from './components/layout/SearchBar';
+import Photo from './components/photos/Photo';
+import PhotoModal from './components/photos/PhotoModal';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Fragment>
+        <Navbar />
+        <div className='container'>
+          <SearchBar />
+          <Photo />
+        </div>
+        <PhotoModal />
+      </Fragment>
+    </Provider>
   );
 }
 
