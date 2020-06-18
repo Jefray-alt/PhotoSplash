c6729731f58d891c6e063d9cd559c80c64eaee63
Jeffrey France <jbfrance1626@gmail.com>
2020-06-18 13:35:40 +0800

    fixed vars again

--- src/actions/photoActions.js
+++ src/actions/photoActions.js
@@ -10,7 +10,7 @@ import {
 let key;
 
 if (process.env.NODE_ENV === 'production') {
-  key = process.env.UNSPLASH_KEY;
+  key = process.env.REACT_APP_UNSPLASH_CLIENT_ACCESS_KEY;
 } else {
   key = process.env.REACT_APP_UNSPLASH_CLIENT_ACCESS_KEY;
 }
